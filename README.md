1502c88ec5be111f8196226f0ae580d6e1f77531
Brian Wagner <briwagner@outlook.com>
2021-12-08 16:21:09 -0600

    add readme; remove logging; add redirect for any other route

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTTP Demo
+
+This is a demo to show how we pull PORT from the environment. In render.com, this seems to be '10000' typically.
+
+Note in Go we have to add the colon, done with Sprintf().
--- main.go
+++ main.go
@@ -11,24 +11,25 @@ var PORT string
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
-	fmt.Printf("PORT is %s\n", os.Getenv("PORT"))
-	fmt.Printf("port is %s\n", os.Getenv("port"))
+	mux.HandleFunc("/", HomeHandler)
 
-	appPort := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	// Set env var with colon ":80".
+	appPort := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	err := http.ListenAndServe(appPort, mux)
 	log.Fatal(err)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		log.Printf("visitor to %s", r.URL.Path)
+
+		http.Redirect(w, r, "/", http.StatusNotFound)
 		return
 	}
 
 	switch r.Method {
 	case http.MethodGet:
+		log.Println("visitor to homepage")
 		w.Write([]byte("This is the home page"))
 
 	case http.MethodPost:
